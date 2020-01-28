1b75658fc00694dd9fda6a2514b33a312f56c6ed
huamar <huamartincode@outlook.com>
2020-01-28 21:51:22 +0000

    add crontab job for data collection from internet

--- cronjob.py
+++ cronjob.py
@@ -2,6 +2,6 @@ from crontab import CronTab
 
 cron = CronTab(user='huazhao')
 job = cron.new(command='python3 /Users/huazhao/altcoins/collectaltcoinData.py')
-job.minute.every(1)
+job.hour.every(1)
 
 cron.write()
